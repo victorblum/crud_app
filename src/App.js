dd37d0b9c797a8ee15044daf13afc5ff9119a09c
Victor <victor@iMac-Sumbel.local>
2021-10-31 19:47:04 +0300

    Add all my files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  render() { 
+    return (
+        <div className="container">
+        <h1 className="nm-3">Orders</h1>
+        <div className="row mb-3">
+          <div className="col-auto">
+            <input type="text" className="form-control" placeholder="new order"/>
+          </div>
+          <div className="col-auto">
+            <button id="addOrder" className="btn btn-primary">Add</button>
+          </div>
+        </div>
+        <div id="orders">
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input"/>
+            Some order
+          </label>
+          <button type="button" class="btn-close" aria-label="Close" style={{fontSize: 10}}></button>
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
-
-export default App;
